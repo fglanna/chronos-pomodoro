0279263b2d12cc2e3d622686fb71bd784047f2db
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2026-09-14 18:15:27 -0300

    Form: useRef, useEffect

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,11 +2,49 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Button } from '../Button';
 import { Cycles } from '../Cycles';
 import { Input } from '../Input';
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
   const handleCreateNewTask = (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('Deu Certo');
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // *conferir
+        secondsRemaining, // *conferir
+        formattedSecondsRemaining: '00:00', // *conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   };
   return (
     <form onSubmit={handleCreateNewTask} className='form' action=''>
@@ -16,6 +54,7 @@ export function MainForm() {
           id='input'
           type='text'
           placeholder='Digite aqui'
+          ref={taskNameInput}
         />
       </div>
       <div className='formRow'>
